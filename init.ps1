b26519c1bfd7d84ab69ad728230ba176fc459f2e
alparamonov <alex.s.paramonov@gmail.com>
2017-01-19 17:09:35 +0100

    init.ps1 script for interface concept

---
+++ init.ps1
@@ -0,0 +1,41 @@
+#################################################
+# Initialization script to start configuration  #
+# of agent in mounted image                     #
+#################################################
+#                                               #
+# The following environment variables are       #
+# expected in the target container              #
+#                                               #
+#    agent_username                             #
+#    agent_password                             #
+#    agent_name                                 #
+#    agent_serverurl                            #
+#    agent_token                                #  
+#                                               #
+#################################################
+
+param (
+    [string]$agent_username,  
+    [SecureString]$agent_password,
+    [string]$agent_name, 
+    [string]$agent_serverurl,
+    [string]$agent_pool,
+    [string]$agent_token     
+)
+
+if ($agent_token -eq $null)
+{
+    & "$env:SystemDrive\cfg\Configure-Agent.ps1" -agent_username $agent_username `
+        -agent_password $agent_password `
+        -agent_name $agent_name `
+        -agent_serverurl $agent_serverurl `
+        -agent_pool $agent_pool 
+}
+
+if ($agent_token -ne $null)
+{
+    & "$env:SystemDrive\cfg\Configure-Agent.ps1" -agent_token $agent_token `
+        -agent_name $agent_name `
+        -agent_serverurl $agent_serverurl `
+        -agent_pool $agent_pool
+}
