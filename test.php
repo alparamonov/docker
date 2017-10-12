19eff48efb1016ec2e5a4fdc3d922a105bf02fa2
Alexey Paramonov <alex.s.paramonov@gmail.com>
2017-10-12 14:34:46 +0200

    Update test.php

--- test.php
+++ test.php
@@ -4,7 +4,8 @@
    // $prod = $_GET['prod'];
    // $ind = mt_rand(1, 3);
    // $path = "http://stor0" . $ind . "haufe.blob.core.windows.net/2017/product-a/zips/" . $prod;
-    $path = "https://stor01haufe.blob.core.windows.net/live/loadtest/product-a/200MB.zip";
+    //$path = "https://stor01haufe.blob.core.windows.net/live/loadtest/product-a/200MB.zip";
+    $path = "http://ipv4.download.thinkbroadband.com/5MB.zip";
     header("HTTP/1.1 301 Moved Permanently");
     header("Location: " . $path);
 
